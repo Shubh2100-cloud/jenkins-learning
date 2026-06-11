942d50eda3bba1cdb030057c3a940bda69b2985f
Shubh2100-cloud <shubhamgoal36@gmail.com>
2026-06-11 13:28:21 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,1 @@
-print("Hello, I'm Shubham and Im creating a jenkins pipeline first time in my career, Dadda adesh") 
+print("Hello, I'm Shubham and Im creating a jenkins pipeline first time in my career, Hanumaan Dadda adesh") 
